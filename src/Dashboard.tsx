4ed2c2678a236e2d2d9e1e62b2363e5df3100c3f
Rahim Klabér <r.e.p.klaber@student.tudelft.nl>
2021-07-28 19:00:29 +0200

    remove ability to look up addresses

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -2,37 +2,46 @@ import React from "react";
 import {AppBar, Button, Container, TextField} from "@material-ui/core";
 import {getAlbedoPublicKey} from "./lib/albedo";
 import "./DashBoard.css"
+import {shorten} from "./lib/utils";
 
 interface IDashBoardProp {
     setAddress: (addr: string, albedo: boolean) => void
 }
 
 interface IDashBoardState {
-    addressFromField: string
+    address: string
+    loggedIn: boolean
 }
 
 export class DashBoard extends React.Component<IDashBoardProp, IDashBoardState> {
     constructor(props: any) {
         super(props);
-        this.state = {addressFromField: ""}
+        this.state = {address: "",loggedIn: false}
     }
 
     render() {
-
+        const albedoButtonText = this.state.loggedIn ? shorten(this.state.address) : "Connect with Albedo"
         return (
             <AppBar position="sticky" className="DashBoard">
                 <Container>
-                    <TextField variant="outlined"
-                               onChange={(event) => this.setState({addressFromField: event.target.value})}/>
-                    <Button color="primary"
-                            onClick={() => this.props.setAddress(this.state.addressFromField, false)}>
-                        <b  className="text">Load</b>
-                    </Button>
+                    {/*<TextField variant="outlined"*/}
+                    {/*           onChange={(event) => this.setState({address: event.target.value})}/>*/}
+                    {/*<Button color="primary"*/}
+                    {/*        onClick={() => this.props.setAddress(this.state.address, false)}>*/}
+                    {/*    <b  className="text">Load</b>*/}
+                    {/*</Button>*/}
 
-                    {/*<div/>*/}
+                    <div className="grow"/>
                     <Button color="primary"
-                            onClick={() => getAlbedoPublicKey().then(address => this.props.setAddress(address, true))}>
-                        <b className="text" >Connect With Albedo</b>
+                            onClick={() => getAlbedoPublicKey().then(address => {
+                                this.props.setAddress(address, true)
+                                this.setState({
+                                    loggedIn : true,
+                                    address : address
+                                })
+                                console.log(this.state.address)
+                            })}>
+                        <b className="text" >{albedoButtonText}</b>
                     </Button>
 
                     <Button color="primary">
