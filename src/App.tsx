95b28d8f5921aca63f3e20a9b91ede22ce302eb5
Rahim Klabér <r.e.p.klaber@student.tudelft.nl>
2021-07-29 21:42:21 +0200

    add query param for address

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,18 @@ class App extends React.Component<any, IAppState> {
     }
 
     componentDidMount() {
-        this.loadBadges("")
+        // eslint-disable-next-line no-restricted-globals
+        const search = location.search
+        const params = new URLSearchParams(search)
+
+        if(params.get("address")!==null){
+            this.loadBadges(params.get("address") as string)
+            this.setState({
+                address: params.get("address") as string
+            })
+        }else{
+            this.loadBadges("")
+        }
     }
 
     updateAddress(address: string, loggedIn: boolean) {
@@ -28,6 +39,26 @@ class App extends React.Component<any, IAppState> {
             loggedIn : loggedIn
         })
         this.loadBadges(address)
+        this.clearPath()
+        if(address !== ""){
+            //todo
+            // eslint-disable-next-line no-restricted-globals
+            const search = location.search
+            const params = new URLSearchParams(search)
+            params.set("address",address)
+            // eslint-disable-next-line no-restricted-globals
+            const link = location.pathname + "?" + params.toString()
+            // eslint-disable-next-line no-restricted-globals
+            history.pushState(null,"",link)
+        }
+
+    }
+    // set path to the base path
+    clearPath(){
+        // eslint-disable-next-line no-restricted-globals
+        const link = location.pathname
+        // eslint-disable-next-line no-restricted-globals
+        history.pushState(null,"",link)
     }
 
     loadBadges(address: string){
@@ -46,6 +77,9 @@ class App extends React.Component<any, IAppState> {
                     <h1 className="title">
                         Stellar Quest badge checker
                     </h1>
+                    <h2 className="text">
+                        {this.state.address}
+                    </h2>
                 </header>
                 <div>
                     <Badges badges={this.state.badges}/>
